4d32882ca8507b5456a1937da92cd1066b0f7ee2
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-10-12 10:18:19 +0100

    actioned some of comments

--- src/main/app/models/draft.ts
+++ src/main/app/models/draft.ts
@@ -3,5 +3,9 @@ import { Moment } from 'moment'
 import { DraftDocument } from 'models/draftDocument'
 
 export class Draft<T extends DraftDocument> {
-  constructor (public id: number = undefined, public type: string, public document: T, public created?: Moment, public updated?: Moment) {}
+  constructor (public id: number = undefined,
+               public type: string,
+               public document: T,
+               public created?: Moment,
+               public updated?: Moment) {}
 }
